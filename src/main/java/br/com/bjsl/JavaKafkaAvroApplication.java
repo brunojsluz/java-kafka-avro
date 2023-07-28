ad21a348d6059d5bfd9800ac9ebe54b47ad4bf09
Bruno Jorge dos Santos Luz <brunoluz@Brunos-MacBook-Pro.local>
2023-07-28 20:07:10 -0300

    First version

---
+++ src/main/java/br/com/bjsl/JavaKafkaAvroApplication.java
@@ -0,0 +1,13 @@
+package br.com.bjsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaAvroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKafkaAvroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bjsl/JavaKakaAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bjsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKakaAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
